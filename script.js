e8d274865d5ceda8eb725cde6b74b72b12c8bf46
AlionaSmirnova <smirnova.qs@gmail.com>
2021-11-19 13:58:12 +0200

    done

---
+++ script.js
@@ -0,0 +1,36 @@
+
+var op;
+function func(){
+    var result;
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    
+    switch (op){
+        case '+': result = num1 + num2;
+             break;
+        case '-': result = num1 - num2;
+             break;
+        case '*': result = num1 * num2;
+              break;
+        case '/': result = num1 / num2;
+             break;
+    }
+    document.getElementById("result").innerHTML = result;
+}
+document.getElementById("reset").onclick = function() {
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = ""; 
+    document.getElementById("result").innerHTML = " "; 
+  };
+// var result='';
+
+// var input = function (char){
+// this.result = this.result.toString();
+// this.result+=char;
+// };
+// var reset = function(){
+// this.result=' ';
+// }
+// function calc(){
+//     this.result = eval(this.result);
+// }
